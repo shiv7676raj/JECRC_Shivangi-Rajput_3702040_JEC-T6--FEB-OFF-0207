ead8d5207473f1c2b71e5a2c95027ce90042c471
shiv7676raj <shivangi.24mcan0245@jecrcu.edu.in>
2026-03-06 13:13:50 +0530

    Delete Day5 directory

--- Day5/Ex1.py
+++
@@ -1,5 +0,0 @@
-#Write a program to create a simple addition function
-def sum(a,b):
-    return a+b
-
-print(sum(10,20))
--- Day5/Ex3.py
+++
@@ -1,3 +0,0 @@
-transactions=[35353,23232,576767,3333,3333,3333]
-s=set(transactions)
-print(s)
